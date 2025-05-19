366f53790bc337c3109933a51425fbc5ae17ca9b
julian-gamboa-bahia <fulanodetal@exemplo.br>
2025-05-18 21:56:08 -0300

    Componente:  modal-login  Que permite fazer login usando uma janela MODAL

--- maio-2025/certificados-2025/src/app/app.routes.ts
+++ maio-2025/certificados-2025/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { NovosComponent } from './components/novos/novos.component';
-import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: NovosComponent },
   { path: ':parametro/:parametro', component: NovosComponent },
-    { path: 'login', component: LoginComponent }
 ];
--- maio-2025/certificados-2025/src/app/components/modal-login/modal-login.component.ts
+++ maio-2025/certificados-2025/src/app/components/modal-login/modal-login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+
 
 @Component({
   selector: 'app-modal-login',
-  imports: [],
+  imports: [AmplifyAuthenticatorModule],
   templateUrl: './modal-login.component.html',
   styleUrl: './modal-login.component.css'
 })
 export class ModalLoginComponent {
+
+  constructor(private readonly modalService: NgbModal) { }
+
+
+  open(content: TemplateRef<any>) {
+
+    console.log('Abrindo modal de login');
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        console.log('fechou o modal de login com:');
+      },
+
+    );
+  }
+
+  logSignOut() {
+    console.log('Usuário deslogado');
+  }
 
 }
